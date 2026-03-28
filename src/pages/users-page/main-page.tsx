5fe39077dd97609f539752e8308e1d43eb236986
Mikhail Zakharov <mike.zakharov1990@gmail.com>
2026-03-28 21:02:55 +0300

    minor styles edits and adaptive main-page

--- src/pages/users-page/main-page.tsx
+++ src/pages/users-page/main-page.tsx
@@ -1,28 +1,27 @@
 import { api } from "../../components/api/api";
-import { useQuery} from '@tanstack/react-query'
+import { useQuery } from "@tanstack/react-query";
 import { UserCard } from "../../components/user-card/user-card";
-import type {User} from "../../../types/user-type"
+import type { User } from "../../../types/user-type";
 import { useUsersStore } from "../../../store/users-store";
-import styles from "./main-page.module.scss"
+import styles from "./main-page.module.scss";
 
-export const MainPage = () =>{
+export const MainPage = () => {
+  function getUsers() {
+    return api.get("users?_limit=6").then((res) => res.data);
+  }
 
-  function getUsers(){
-  return api.get("users?_limit=6").then((res) => res.data)
-  } 
-
-  const {data: users = []} = useQuery({
+  const { data: users = [] } = useQuery({
     queryKey: ["users"],
-    queryFn: getUsers
-  })
+    queryFn: getUsers,
+  });
 
-  const { statuses, setStatus } = useUsersStore();
+  const { statuses } = useUsersStore();
 
   const activeUsers = users.filter(
-    (user:User) => (statuses[user.id] ?? "active") === "active"
+    (user: User) => (statuses[user.id] ?? "active") === "active",
   );
   const archiveUsers = users.filter(
-    (user:User) => (statuses[user.id] ?? "active") === "archive"
+    (user: User) => (statuses[user.id] ?? "active") === "archive",
   );
 
   return (
@@ -32,34 +31,38 @@ export const MainPage = () =>{
           <h2 className={styles.title}>Активные</h2>
         </div>
         <div className={styles.container}>
-          {activeUsers?.map((user:User) => {
-            return <UserCard 
-              key={user.id}
-              id={user.id}
-              name = {user.name}
-              city = {user.address.city}
-              company={user.company.name}
-            />
+          {activeUsers?.map((user: User) => {
+            return (
+              <UserCard
+                key={user.id}
+                id={user.id}
+                name={user.name}
+                city={user.address.city}
+                company={user.company.name}
+              />
+            );
           })}
         </div>
       </section>
-        
+
       <section>
         <div className={styles.header}>
           <h2 className={styles.title}>Архив</h2>
         </div>
         <div className={styles.container}>
-          {archiveUsers?.map((user:User) => {
-            return <UserCard 
-              key={user.id}
-              id={user.id}
-              name = {user.name}
-              city = {user.address.city}
-              company={user.company.name}
-            />
+          {archiveUsers?.map((user: User) => {
+            return (
+              <UserCard
+                key={user.id}
+                id={user.id}
+                name={user.name}
+                city={user.address.city}
+                company={user.company.name}
+              />
+            );
           })}
         </div>
       </section>
     </main>
-  )
-}
+  );
+};
