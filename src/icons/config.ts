eca6bbf3840924f42b2cf0120ff9fd84a8950f17
Mikhail Zakharov <mike.zakharov1990@gmail.com>
2026-03-28 19:24:49 +0300

    refactoring header, add icons favorite and notification

--- src/icons/config.ts
+++ src/icons/config.ts
@@ -1,13 +1,16 @@
 import { CrossIcon } from "./assets/cross-icon";
+import { FavoriteIcon } from "./assets/favorite";
 import { MenuDotsIcon } from "./assets/menu-dots";
+import { NotificationIcon } from "./assets/notification";
 import type { IconProps } from "./icons-type";
-
 
 export type Icons = {
   [key: string]: React.FC<IconProps>;
 };
 
-export const ICONS_MAP: Icons ={
-  cross : CrossIcon,
-  "menu-dots": MenuDotsIcon
+export const ICONS_MAP: Icons = {
+  cross: CrossIcon,
+  "menu-dots": MenuDotsIcon,
+  favorite: FavoriteIcon,
+  notification: NotificationIcon,
 };
